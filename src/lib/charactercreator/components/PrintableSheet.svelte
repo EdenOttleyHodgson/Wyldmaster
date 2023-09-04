5dc9b8b4e7fbb42a151def9b8b73149c8d977491
EdenOttleyHodgson <lh12g22@soton.ac.uk>
2023-09-04 16:21:04 +0100

    Finished first page of printable character sheet

--- src/lib/charactercreator/components/PrintableSheet.svelte
+++ src/lib/charactercreator/components/PrintableSheet.svelte
@@ -1,6 +1,6 @@
 <div class="page-box">
     <div class="printable">
-        <div style="height: 10%; border: 1px solid black; display:flex; align-items:center">
+        <div class="bottom-border" style="height: 10%; display:flex; align-items:center">
             <div class="section">
                 <div class = info-div style="width: 40%; height: 40%">
                     <div class ="write-in" style="width: 95%; min-height: 50%"></div>
@@ -32,9 +32,230 @@
             </div>
         </div>
 
-        <div style="height: 5%; border: 1px solid black"></div>
+        <div style="height: 5%; display: flex; align-items: center; justify-content: space-between; padding-bottom: 2mm" class="bottom-border">
+            <div style="border: 1px solid black; min-width: 15%; height: 95%; text-align: center">
+                Stamina
+            </div>
+                
+            <div style="border: 1px solid black; width: 10%; height: 95%; display: flex; align-items: center; justify-content: center; flex-direction: column">
+                AP
+                <div style="display: flex;">
+                    <div id="circle"></div>
+                    <div id="circle"></div>
+                    <div id="circle"></div>
+                    <div id="circle"></div>
+                </div>
+            </div>
+
+                <div class="stats-box">
+                    Melee Prof.
+                </div>
+                <div class="stats-box">
+                    Ranged Prof.
+                </div>
+                <div class="stats-box">
+                    Magic Prof.
+                </div>
+                <div class="stats-box"> 
+                    Dodge
+                </div>
+                <div class="stats-box"> 
+                    Reslience
+                </div>
+                <div class="stats-box">
+                    Magical Defence
+                </div>
+            </div>
         
-        <div style="height: 90%; border: 1px solid black">bottom bar</div>
+        <div style="height: 90%; display: flex; flex-direction: column; margin-left:2px;">
+            <div style="width: 99%; height: 30%; margin-top: -2mm; padding-bottom: 2mm" class="bottom-border">
+                <h3 style="text-align: center">Wounds</h3>
+                <table style="width: 100.2%; height:78%">
+                    <tr>
+                        <th style="width: 20%;">Severity</th>
+                        <th style="width: 22.5%;">Name</th>
+                        <th>Effect</th>
+                    </tr>
+                    <tr>
+                        <td>1: Minor</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td>2: Moderate</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    
+                    <tr>
+                        <td>3: Major</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    
+                    <tr>
+                        <td>4: Lethal</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+            <div style="width: 100.2%; height: 70%; display: flex; margin: 0 !important; justify-content: space-between;">
+                <div style="border: 1px solid black; width: 100%;">
+                    <h3 style="text-align: center;">Weapons</h3>
+                    <table style="height: 40%;">
+                        <tr>
+                            <th style="width: 40%;">Name</th>
+                            <th>Damage</th>
+                            <th>Range</th>
+                            <th>Weight</th>
+                            <th style="width: 8%;">AP Cost</th>
+                            <th style="width: 40%;">Tags</th>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                    <h3 style="text-align: center;">Armour</h3>
+                    <table style="height: 40%;">
+                        <tr>
+                            <th style="width: 40%;">Name</th>
+                            <th>Weight</th>
+                            <th>Resilience</th>
+                            <th>Dodge</th>
+                            <th>Stamina Reduction</th>
+                            <th style="width: 40%;">Tags</th>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
         
     </div>
     <div class="printable">
@@ -137,5 +358,28 @@
        width: 100%;
        height: 100%;
        margin: -1mm;
+    } 
+    #circle {
+      width: 10px;
+      height: 10px;
+      -webkit-border-radius: 25px;
+      -moz-border-radius: 25px;
+      border-radius: 25px;
+      border: 1px solid black
+    }
+    .stats-box{
+        border: 1px solid black;
+        width: 100%;
+        height: 95%;
+        font-size: x-small;
+        text-align: center;
+        margin: -1mm;
+    }
+    .bottom-border{
+        border-bottom: 2px solid black;
+    }
+    th, td, table{
+        border: 1px solid black;
+        border-collapse: collapse
     }
 </style>
